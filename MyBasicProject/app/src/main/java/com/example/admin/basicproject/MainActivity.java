c13db080c3d9d2ee844261f4e1eb13692dfa77aa
jspfei <jiangfeifan8@163.com>
2016-12-06 16:09:41 +0800

    生成主界面

--- MyBasicProject/app/src/main/java/com/example/admin/basicproject/MainActivity.java
+++ MyBasicProject/app/src/main/java/com/example/admin/basicproject/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.admin.basicproject;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    int[] mainImageIds = {R.drawable.icon_0,R.drawable.icon_1,R.drawable.icon_2};
+    String[] mainStrs = {"UI","Jar","MVP"};
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
+
+        GridView gridView = (GridView) findViewById(R.id.gv_show);
+        //动态生成数组，传入数据
+        ArrayList<HashMap<String,Object>> listItem = new ArrayList<HashMap<String, Object>>();
+        for(int i = 0 ;i<mainImageIds.length;i++){
+            HashMap<String,Object> map = new HashMap<String,Object>();
+            map.put("itemsImg",mainImageIds[i]);
+            map.put("itemsStr",mainStrs[i]);
+            listItem.add(map);
+        }
+
+        SimpleAdapter imageItems = new SimpleAdapter(this,listItem,R.layout.grid_items,new String[]{"itemsImg","itemsStr"},
+                new int[]{R.id.itemImage,R.id.itemText});
+        gridView.setAdapter(imageItems);
+
+        //点击图片进入响应操作
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i){
+                    case 0:
+                        Toast.makeText(mContext,"UI",Toast.LENGTH_LONG).show();
+                        break;
+                    case 1:
+                        Toast.makeText(mContext,"jar",Toast.LENGTH_LONG).show();
+                        break;
+                    case 2:
+                        Toast.makeText(mContext,"MVP",Toast.LENGTH_LONG).show();
+                        break;
+
+                }
+            }
+        });
     }
 }
