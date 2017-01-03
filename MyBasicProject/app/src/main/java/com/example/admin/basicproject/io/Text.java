849364a97d69913d8f467f6d55443842d00ef0b3
jspfei <jiangfeifan8@163.com>
2017-01-03 10:41:10 +0800

    shuju

---
+++ MyBasicProject/app/src/main/java/com/example/admin/basicproject/io/Text.java
@@ -0,0 +1,8 @@
+package com.example.admin.basicproject.io;
+
+/**
+ * Created by admin on 2016/12/30.
+ */
+
+public class Text {
+}
---
+++ MyBasicProject/app/src/test/java/com/example/admin/basicproject/io/TextTest.java
@@ -0,0 +1,22 @@
+package com.example.admin.basicproject.io;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by admin on 2016/12/30.
+ */
+public class TextTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void all() throws Exception {
+
+    }
+
+}
